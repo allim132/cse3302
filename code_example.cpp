2f50c2a278c9c4ba021bfe9c7aec5562e4d11316
Alex Nguyen <alexmnguyen2005@gmail.com>
2025-04-13 22:04:11 -0500

    Lab 4

---
+++ code_example.cpp
@@ -0,0 +1,17 @@
+class MyClass {
+	// Members here
+}; // Semicolon here
+
+
+void myFunction() {
+	// Function body
+} // No Semicolon here
+
+
+#define LOG(message) std::cout << message << std::endl
+
+void myFunction() {
+	LOG("This uses a macro.");
+}
+
+
---
+++ java_example.java
@@ -0,0 +1,21 @@
+public class java_example {
+	
+
+
+
+
+
+	Animal dog = new Dog();
+	Animal cat = new Cat();
+	Animal person = new Human();
+
+	ArrayList<Animal> list = new ArrayList<Animal>();
+
+	list.add(dog);
+	list.add(cat);
+	list.add(person);
+
+
+
+
+}
